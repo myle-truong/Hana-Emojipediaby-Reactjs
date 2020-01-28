1821b627e43188c44d66f607058f315824ce8ff3
myle-truong <56028880+myle-truong@users.noreply.github.com>
2020-01-28 14:46:26 +0100

    update emojipedia.js

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -3,7 +3,7 @@ const emojipedia = [
     id: 1,
 
     src: '../img/1.jpg',
-    name: 'Smiling Face With Heart-Eyes',
+    name: 'Heart-Eyes',
     meaning:
       'Happily beaming face with heart-shaped eyes. Insanely in love: with a person, a place or an object. Expresses the extent of love and affection as well as gratitude and is often part of romantic messages. Just does not know what to do with all the luck.'
   },
@@ -11,17 +11,23 @@ const emojipedia = [
     id: 2,
 
     src: '../img/2.jpg',
-    name: 'Face with party blower and party hat',
+    name: 'Winking Face',
     meaning:
-      "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
+      'A face with a slight smile shown winking, usually its left eye. May signal a joke, flirtation, hidden meaning, or general positivity. Tone varies, including playful, affectionate, suggestive, or ironic.'
   },
   {
     id: 3,
-
     src: '../img/3.jpg',
-    name: 'Rolling On The Floor, Laughing',
+    name: 'Face with Tears of Joy',
     meaning:
-      'This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.'
+      'A face with a big grin, uplifted eyebrows, and smiling eyes, each shedding a tear from laughing so hard. Widely used to show something is funny or pleasing.'
+  },
+  {
+    id: 4,
+    src: '../img/4.jpg',
+    name: 'Wow Face',
+    meaning:
+      'A face with a broad, open smile, showing upper teeth on most platforms, with stars for eyes, as if seeing a beloved celebrity. Color of stars widely varies by platform, though commonly gold or red. May express that someone or something is amazing, fascinating, impressive, or exciting'
   }
 ];
 export default emojipedia;
